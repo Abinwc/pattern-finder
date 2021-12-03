70d461fea27970184080ca43a2243678c1200e8c
abin <abin@surfboard.se>
2021-12-03 23:57:37 +0530

    formatted

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const sort = require("./ranking-algorithm");
 const patternCreator = require("./pattern-creator");
 const fileLocation = "./sample3.txt";
 const pattern = "abin";
-const fs= require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function responseHandler(data, pattern) {
-	data= data.trim();
+	data = data.trim();
 	if (data.length == 0) {
 		console.log(`No matching patterns found.`);
 		return;
@@ -14,10 +14,9 @@ function responseHandler(data, pattern) {
 
 	const groupedData = groupData(data, pattern);
 
-	const rankOneList= resultRanker(groupedData.family, pattern);
-  const rankTwoList= resultRanker(groupedData.closeFriends, pattern);
-	const rankThreeList= resultRanker(groupedData.friends, pattern);
-
+	const rankOneList = resultRanker(groupedData.family, pattern);
+	const rankTwoList = resultRanker(groupedData.closeFriends, pattern);
+	const rankThreeList = resultRanker(groupedData.friends, pattern);
 
 	console.log("Ranked list for exact patterns");
 	console.log(rankOneList);
@@ -25,82 +24,72 @@ function responseHandler(data, pattern) {
 	console.log(rankTwoList);
 	console.log("Ranked list for patterns with two letter mismatch");
 	console.log(rankThreeList);
-	
 
-//TODO: NO TIME WRITING FILE EFFECTIVELY	
-	
-//	 fs.open('ranked-list.txt', 'r', function (err, f) {
-//      if(err) {
-//				fs.mkdir('ranked-list.txt', (err) => {
-//    if (err) {
-//        return console.error(err);
-//    }
-//    console.log('Directory created successfully!');
-//});
-//			}
-//});
+	//TODO: NO TIME WRITING FILE EFFECTIVELY
 
-//fs.writeFile("ranked-list.txt", rankOneList.join("\n"), (err) => {
-//  if (err)
-//    console.log(err);
-//  else {
-//    console.log("File written successfully\n");
-//  }
-//});
+	//	 fs.open('ranked-list.txt', 'r', function (err, f) {
+	//      if(err) {
+	//				fs.mkdir('ranked-list.txt', (err) => {
+	//    if (err) {
+	//        return console.error(err);
+	//    }
+	//    console.log('Directory created successfully!');
+	//});
+	//			}
+	//});
 
+	//fs.writeFile("ranked-list.txt", rankOneList.join("\n"), (err) => {
+	//  if (err)
+	//    console.log(err);
+	//  else {
+	//    console.log("File written successfully\n");
+	//  }
+	//});
 }
 
-function groupData(data, pattern){
-   const arr = data.split(/\n/);
-   const pat = pattern.toLowerCase();
+function groupData(data, pattern) {
+	const arr = data.split(/\n/);
+	const pat = pattern.toLowerCase();
 
 	//Array Family stores the patterns that directly matches with the original pattern.
 	const family = [];
 	//Array CloseFriends stores the patterns that is one word different from the original pattern.
 	const closeFriends = [];
 	//Array Friends stores the patterns that is two word different from the original pattern.
-	const friends = []; 
+	const friends = [];
 
-	 for(let i =0; i< arr.length ; i++){
-		 const element = arr[i].toLowerCase();
+	for (let i = 0; i < arr.length; i++) {
+		const element = arr[i].toLowerCase();
 
-		 if(element == pat)
-			 family.push(arr[i]);
-		 else {
-			 const elementChars = element.split("");
-			 const patternChars = pat.split("");
-			 let diffCounter = 0;
-			 for(let i =0 ; i< elementChars.length; i++){
-				 if(elementChars[i] != patternChars[i])
-					 diffCounter++;
-			 }
-			 if(diffCounter ==1)
-				 closeFriends.push(arr[i]);
-			 else 
-			   friends.push(arr[i]);
-		 }
-		
-	 }
+		if (element == pat) family.push(arr[i]);
+		else {
+			const elementChars = element.split("");
+			const patternChars = pat.split("");
+			let diffCounter = 0;
+			for (let i = 0; i < elementChars.length; i++) {
+				if (elementChars[i] != patternChars[i]) diffCounter++;
+			}
+			if (diffCounter == 1) closeFriends.push(arr[i]);
+			else friends.push(arr[i]);
+		}
+	}
 
-   return {
-		 family: family,
-		 closeFriends: closeFriends,
-		 friends: friends
-	 }
-
+	return {
+		family: family,
+		closeFriends: closeFriends,
+		friends: friends,
+	};
 }
 
-
 function resultRanker(data, pattern) {
-
-/**
- * Ranking Algorithm explanation:
- *
- *There are two possible cases(upper-case and lower case) for a particular letter at a position in the pattern. 
- * That particular case for that particular letter at that position get a value. 
- *
- * TODO: Finish the algorithm explanation
- */
+	/**
+	 * Ranking Algorithm explanation:
+	 *
+	 *There are two possible cases(upper-case and lower case) for a particular letter at a position in the pattern.
+	 * That particular case for that particular letter at that position get a value.
+	 *
+	 * TODO: Finish the algorithm explanation
+	 */
 	const trimmedPattern = pattern.trim();
 	const searchResults = data;
 
@@ -118,31 +107,29 @@ function resultRanker(data, pattern) {
 
 	/**
 	 * RankValue array and the Search Results are sent for sorting
-	 */ 
-	sort(rankValue, searchResults, 0, rankValue.length-1);
+	 */
+	sort(rankValue, searchResults, 0, rankValue.length - 1);
 
 	return searchResults;
 }
 
-
-
 function patternFinder(file, pattern, done) {
-try{  
-	const spawn = require("child_process").spawn;
-	let res = "";
+	try {
+		const spawn = require("child_process").spawn;
+		let res = "";
 
-	const patternToMatch = patternCreator(pattern);
+		const patternToMatch = patternCreator(pattern);
 
-	const child = spawn("grep", ["-ioE", patternToMatch, file]);
-	child.stdout.on("data", function (buffer) {
-		res += buffer.toString();
-	});
-	child.stdout.on("end", function () {
-		done(res, pattern);
-	});
-}catch(e){
-	console.log(`Errored occured while searching pattern ${e.message}`);
-}
+		const child = spawn("grep", ["-ioE", patternToMatch, file]);
+		child.stdout.on("data", function (buffer) {
+			res += buffer.toString();
+		});
+		child.stdout.on("end", function () {
+			done(res, pattern);
+		});
+	} catch (e) {
+		console.log(`Errored occured while searching pattern ${e.message}`);
+	}
 }
 
 patternFinder(fileLocation, pattern, responseHandler);
